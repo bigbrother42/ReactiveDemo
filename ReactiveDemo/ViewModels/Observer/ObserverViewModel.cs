3ffe7b9687c564d48544136e7bccb2f1835dc46b
bigbrother42 <garrett427738@outlook.com>
2023-06-29 17:54:59 +0800

    Subject Demo

--- ReactiveDemo/ViewModels/Observer/ObserverViewModel.cs
+++ ReactiveDemo/ViewModels/Observer/ObserverViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace ReactiveDemo.ViewModels.Observer
+﻿using System;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace ReactiveDemo.ViewModels.Observer
 {
     public class ObserverViewModel : ViewModelBase
     {
         #region Models
 
-        
+
 
         #endregion
 
@@ -25,6 +29,10 @@
         protected override void InitData()
         {
             base.InitData();
+
+            // https://zhuanlan.zhihu.com/p/486899755
+
+            SubjectTest1();
         }
 
         protected override void RegisterCommands()
@@ -46,7 +54,127 @@
 
         #region Functions
 
+        #region Subject
 
+        private void SubjectTest1()
+        {
+            var subject = new Subject<int>();
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnNext(3);
+            subject.OnNext(4);
+        }
+
+        private void SubjectTest2()
+        {
+            var subject = new Subject<int>();
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(3);
+            subject.OnNext(4);
+        }
+
+        #endregion
+
+        #region ReplaySubject
+
+        private void ReplaySubjectTest1()
+        {
+            var subject = new ReplaySubject<int>();
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(3);
+            subject.OnNext(4);
+        }
+
+        private void ReplaySubjectTest2()
+        {
+            var subject = new ReplaySubject<int>(1);
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(3);
+            subject.OnNext(4);
+        }
+
+        private void ReplaySubjectTest3()
+        {
+            var subject = new ReplaySubject<int>(TimeSpan.FromMilliseconds(1000));
+            subject.OnNext(1);
+            Thread.Sleep(500);
+            subject.OnNext(2);
+            Thread.Sleep(200);
+            subject.OnNext(3);
+            Thread.Sleep(500);
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(4);
+            Thread.Sleep(500);
+        }
+
+        #endregion
+
+        #region BehaviorSubject
+
+        private void BehaviorSubjectTest1()
+        {
+            var subject = new BehaviorSubject<int>(0);
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(3);
+            subject.OnNext(4);
+        }
+
+        private void BehaviorSubjectTest2()
+        {
+            var subject = new BehaviorSubject<int>(99);
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnNext(3);
+            subject.OnNext(4);
+        }
+
+        private void BehaviorSubjectTest3()
+        {
+            var subject = new BehaviorSubject<int>(0);
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnNext(3);
+            subject.OnNext(4);
+            subject.OnCompleted();
+            subject.Subscribe(Console.WriteLine);
+        }
+
+        #endregion
+
+        #region AsyncSubject
+
+        private void AsyncSubjectTest1()
+        {
+            var subject = new AsyncSubject<int>();
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnNext(3);
+            subject.OnNext(4);
+            subject.OnCompleted();
+        }
+
+        private void AsyncSubjectTest2()
+        {
+            var subject = new AsyncSubject<int>();
+            subject.Subscribe(Console.WriteLine);
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnNext(3);
+            subject.OnNext(4);
+        }
+
+        #endregion
 
         #endregion
     }
