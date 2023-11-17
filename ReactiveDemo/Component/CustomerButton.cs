d7142ed5a92d508a769cf5209889c036a3c94d1c
bigbrother42 <garrett427738@outlook.com>
2023-11-17 19:10:48 +0800

    Customer Component

---
+++ ReactiveDemo/Component/CustomerButton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ReactiveDemo.Component
+{
+    public class CustomerButton : Button
+    {
+        static CustomerButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomerButton), new FrameworkPropertyMetadata(typeof(CustomerButton)));
+        }
+
+        #region DependencyProperty
+
+        public static readonly DependencyProperty CornerRadiusProperty
+            = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(CustomerButton),new PropertyMetadata(null));
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        #endregion
+
+        public CustomerButton()
+        {
+            
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+
+        }
+    }
+}
---
+++ ReactiveDemo/Component/SharedResourceDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReactiveDemo.Component
+{
+    public class SharedResourceDictionary : ResourceDictionary
+    {
+        public static Dictionary<Uri, ResourceDictionary> SharedDictinaries = new Dictionary<Uri, ResourceDictionary>();
+
+        private Uri _sourceUri;
+
+        private static bool IsInDesignMode =>
+            (bool)DependencyPropertyDescriptor.FromProperty(DesignerProperties.IsInDesignModeProperty,
+                typeof(DependencyObject)).Metadata.DefaultValue;
+
+        public new Uri Source
+        {
+            get => IsInDesignMode ? base.Source : _sourceUri;
+            set
+            {
+                _sourceUri = value;
+
+
+                if (!SharedDictinaries.ContainsKey(value))
+                {
+                    try
+                    {
+
+                        base.Source = value;
+                    }
+                    catch (Exception exp)
+                    {
+
+                        if (!IsInDesignMode)
+                            throw;
+                    }
+
+                    SharedDictinaries.Add(value, this);
+                }
+                else
+                {
+
+                    MergedDictionaries.Add(SharedDictinaries[value]);
+                }
+            }
+        }
+    }
+}
