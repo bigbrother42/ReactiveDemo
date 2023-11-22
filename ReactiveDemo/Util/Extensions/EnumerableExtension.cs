b8b372bb6418428fbd32340b17afbdcb9d8db394
bigbrother42 <garrett427738@outlook.com>
2023-11-22 17:56:25 +0800

    Extensions

---
+++ ReactiveDemo/Util/Extensions/EnumerableExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveDemo.Util.Extensions
+{
+    public static class EnumerableExtension
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> self) => self == null || !self.Any();
+
+        public static bool AnyAndNotNull<T>(this IEnumerable<T> self) => !self.IsNullOrEmpty();
+
+        public static List<T> ToListOrEmpty<T>(this IEnumerable<T> self) =>
+            self?.ToList() ?? Enumerable.Empty<T>().ToList();
+
+        public static T[] ToArrayOrEmpty<T>(this IEnumerable<T> self) => self?.ToArray() ?? Array.Empty<T>();
+    }
+}
---
+++ ReactiveDemo/Util/Extensions/ObservableCollectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveDemo.Util.Extensions
+{
+    public static class ObservableCollectionExtension
+    {
+        public static ObservableCollection<T> SetOrUpdate<T>(this ObservableCollection<T> self, IEnumerable<T> values)
+        {
+            if (self == null)
+            {
+                self = new ObservableCollection<T>();
+            }
+            else
+            {
+                self.Clear();
+            }
+
+            if (values.AnyAndNotNull())
+            {
+                self.AddRange(values);
+            }
+
+            return self;
+        }
+    }
+}
